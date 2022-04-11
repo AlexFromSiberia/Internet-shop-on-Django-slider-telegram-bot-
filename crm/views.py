cd97232bca6138aa3b0b7c63f4acff5c1f32a40c
Alex <id764g@gmail.com>
2022-04-11 14:47:44 +0700

    Second version

--- crm/views.py
+++ crm/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import OrderForms
+from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
+
 
 def first_page(request):
-    object_list = Order.objects.all()
-    return render(request, './index.html', {'object_list': object_list})
+    slider_list = CmsSlider.objects.all()
+    price_table = PriceTable.objects.all()
+    form = OrderForms()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    pc_4 = PriceCard.objects.get(pk=4)
+    dict1 = {'slider_list': slider_list,
+             'price_table': price_table,
+             'pc_1': pc_1,
+             'pc_2': pc_2,
+             'pc_3': pc_3,
+             'pc_4': pc_4,
+             'form': form}
+    return render(request, './index.html', dict1)
 
 
 def thanks_page(request):
@@ -11,4 +28,4 @@ def thanks_page(request):
     phone = request.POST['phone']
     data = Order(order_name=name, order_phone=phone)
     data.save()
-    return render(request, './thanks_page.html', {'name': name, 'phone': phone})
+    return render(request, './thanks_page.html', {'name': name})
--- landingpage_website/settings.py
+++ landingpage_website/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crm',
+    'cms',
+    'price',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
