0b5c6506f7a3a8e67c6f17fdbec5da8aebd74a81
AlexFromSiberia <id764g@gmail.com>
2022-10-17 14:39:01 +0700

    Добавил : - Настройка Админ панели (cms app - добавлены миниатюры изображений)

--- cms/admin.py
+++ cms/admin.py
@@ -7,6 +7,8 @@ class CmsAdmin(admin.ModelAdmin):
     list_display = ('cms_title', 'cms_text', 'cms_css', 'get_photo')
     list_display_links = ('cms_title',)
     list_editable = ('cms_css',)
+    fields = ('cms_title', 'cms_text', 'cms_css', 'cms_img', 'get_photo')
+    readonly_fields = ('get_photo', )
 
     # функция для отображения миниатюр изображений в админке
     # import mark_safe !
@@ -14,7 +16,7 @@ class CmsAdmin(admin.ModelAdmin):
     def get_photo(self, obj):
         # избежать ошибки если фото не существует
         if obj.cms_img:
-            return mark_safe(f'<img src="{obj.cms_img.url}" width="100">')
+            return mark_safe(f'<img src="{obj.cms_img.url}" width="150">')
         else:
             return '-'
 
